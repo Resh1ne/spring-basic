7f8e893f87ea8b273051a258289f0460f754ea9b
Daniil Pototskiy <daniilpototskiy@mail.ru>
2025-05-29 21:57:00 +0300

    Added class spring configuration

---
+++ src/main/java/org/example/AppConfig.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/settings.properties")
+public class AppConfig {
+
+    @Bean
+    public Dog dog(@Value("${dog.name}") String name) {
+        Dog dog = new Dog();
+        dog.setName(name);
+        return dog;
+    }
+
+    @Bean
+    public Person person(Dog dog) {
+        return new Person(dog);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/appConfig.xml");
-        Person person = context.getBean("person", Person.class);
+        ClassPathXmlApplicationContext contextXML = new ClassPathXmlApplicationContext("/appConfig.xml");
+        AnnotationConfigApplicationContext contextClass = new AnnotationConfigApplicationContext(AppConfig.class);
+        Person person = contextClass.getBean("person", Person.class);
         person.doSome();
         }
     }
